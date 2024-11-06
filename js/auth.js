ff3c47aed6260808f2dd7bc8d1438fdaf46ad0ca
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-11-05 19:23:42 -0700

    Update auth.js
    did this do anything

--- js/auth.js
+++ js/auth.js
@@ -1,4 +1,4 @@
-import { createAuth0Client } from '@auth0/auth0-spa-js';
+import { createAuth0Client } from 'https://cdn.jsdelivr.net/npm/@auth0/auth0-spa-js@1.21.0/dist/auth0-spa-js.production.js';
 
 let auth0Client = null;
 
