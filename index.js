7a6f5a1547ad7e014bfc121275f29369d50a487f
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-11-07 19:29:57 -0700

    Update index.js
    dont serve admin.html as a static file

--- index.js
+++ index.js
@@ -9,12 +9,13 @@ const port = process.env.PORT || 3000;
 
 app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.get('/pages/admin.html', jwt({
+const jwtCheck = jwt({
     secret: jwksRsa.expressJwtSecret({
         cache: true,
         rateLimit: true,
@@ -24,29 +25,22 @@ app.get('/pages/admin.html', jwt({
     audience: process.env.AUTH0_AUDIENCE,
     issuer: `https://${process.env.AUTH0_DOMAIN}/`,
     algorithms: ['RS256']
-}), (req, res) => {
+});
+
+app.get('/admin', jwtCheck, (req, res) => {
     const claims = req.user;
-    console.log('User claims:', claims);
     const isAdmin = claims && claims['https://mo-bank.vercel.app/isAdmin'];
     if (isAdmin) {
         res.sendFile(path.join(__dirname, 'pages/admin.html'));
     } else {
-        console.log('Access denied: Admins only');
         res.status(403).send('Forbidden: Admins only');
     }
 });
 
-app.use('/pages', (req, res, next) => {
-    if (req.path === '/admin.html') {
-        next();
-    } else {
-        express.static(path.join(__dirname, 'pages'))(req, res, next);
-    }
-});
+app.use('/pages', express.static(path.join(__dirname, 'pages')));
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
-        console.log('Invalid token');
         res.status(401).send('Invalid token');
     } else {
         next(err);
