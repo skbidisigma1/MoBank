68c036d3b65c33fb0bdac0d7037e7b6d314d485e
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-11-07 18:29:42 -0700

    Update index.js
    added console logging

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ app.use('/css', express.static(path.join(__dirname, 'css')));
 app.use('/js', express.static(path.join(__dirname, 'js')));
 app.use('/pages', express.static(path.join(__dirname, 'pages')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 app.get('/pages/admin.html', jwt({
     secret: jwksRsa.expressJwtSecret({
         cache: true,
@@ -24,16 +28,19 @@ app.get('/pages/admin.html', jwt({
     algorithms: ['RS256']
 }), (req, res) => {
     const claims = req.user;
+    console.log('User claims:', claims);
     const isAdmin = claims && claims['https://mo-bank.vercel.app/isAdmin'];
     if (isAdmin) {
         res.sendFile(path.join(__dirname, 'pages/admin.html'));
     } else {
+        console.log('Access denied: Admins only');
         res.status(403).send('Forbidden: Admins only');
     }
 });
 
 app.use((err, req, res, next) => {
     if (err.name === 'UnauthorizedError') {
+        console.log('Invalid token');
         res.status(401).send('Invalid token');
     } else {
         next(err);
