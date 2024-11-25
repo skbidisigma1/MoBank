d05c7a6437becb31800b797655a97964b16e8aee
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-11-25 16:53:55 -0700

    Update index.js
    s

--- index.js
+++ index.js
@@ -82,7 +82,8 @@ const jwtCheck = jwt({
 });
 
 app.use(jwtCheck);
-app.use('/api', userRateLimiter);
+
+//app.use('/api', userRateLimiter);
 
 async function addUser(uid, email, metadata = {}) {
   const userRef = db.collection('users').doc(uid);
