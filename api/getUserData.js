03ecab7f758111792869c236941b6fd5232a2a1c
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-11-25 16:53:34 -0700

    Update getUserData.js
    m

--- api/getUserData.js
+++ api/getUserData.js
@@ -24,9 +24,25 @@ module.exports = async (req, res) => {
     return res.status(405).json({ message: 'Method not allowed' });
   }
 
-  const uid = req.auth.payload.sub;
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  const token = authHeader.split(' ')[1];
 
   try {
+    const response = await fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    if (!response.ok) {
+      return res.status(401).json({ message: 'Token verification failed' });
+    }
+
+    const user = await response.json();
+    const uid = user.sub;
+
     const userRef = db.collection('users').doc(uid);
     const userDoc = await userRef.get();
 
