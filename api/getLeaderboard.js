8f0e025b60dad84fa47c9954b7e0e8e2096896d8
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-12-06 16:37:43 -0700

    Update getLeaderboard.js

--- api/getLeaderboard.js
+++ api/getLeaderboard.js
@@ -1,29 +1,11 @@
-const admin = require('firebase-admin');
 const { expressjwt: jwt } = require('express-jwt');
 const jwksRsa = require('jwks-rsa');
 const express = require('express');
 require('dotenv').config();
 
+const { admin, db } = require('../firebase');
+
 const app = express();
-
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      type: process.env.FIREBASE_TYPE,
-      project_id: process.env.FIREBASE_PROJECT_ID,
-      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-      private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
-      client_email: process.env.FIREBASE_CLIENT_EMAIL,
-      client_id: process.env.FIREBASE_CLIENT_ID,
-      auth_uri: process.env.FIREBASE_AUTH_URI,
-      token_uri: process.env.FIREBASE_TOKEN_URI,
-      auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
-      client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
-    }),
-  });
-}
-
-const db = admin.firestore();
 
 const jwtCheck = jwt({
   secret: jwksRsa.expressJwtSecret({
