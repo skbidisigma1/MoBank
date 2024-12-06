382056b25002407714e9df4b4d43ed255e50dd3d
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-12-05 17:32:39 -0700

    Update aggregateLeaderboard.js
    same

--- api/aggregateLeaderboard.js
+++ api/aggregateLeaderboard.js
@@ -42,7 +42,7 @@ module.exports = async (req, res) => {
     }
 
     const user = await response.json();
-    const roles = user['https://mo-bank.vercel.app/roles'] || [];
+    const roles = user['https://mo-classroom.us/roles'] || [];
 
     if (!roles.includes('admin')) {
       return res.status(403).json({ message: 'Forbidden: Admins only' });
