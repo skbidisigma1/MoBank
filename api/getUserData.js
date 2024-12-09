6804855c6d1ed6746317ad63a540b9f922fa6049
skbidisigma1 <luke.c309@stu.nebo.edu>
2024-12-08 20:52:08 -0700

    Update getUserData.js
    today i made a graham cracker gingerbread house it was so fire

--- api/getUserData.js
+++ api/getUserData.js
@@ -1,43 +1,66 @@
-const { admin, db } = require('../firebase');
+const jwt = require('jsonwebtoken')
+const jwksClient = require('jwks-rsa')
+const { admin, db } = require('../firebase')
+
+const client = jwksClient({
+  jwksUri: `https://${process.env.AUTH0_DOMAIN}/.well-known/jwks.json`
+})
+
+function getKey(header, callback) {
+  client.getSigningKey(header.kid, function (err, key) {
+    if (err) {
+      callback(err)
+    } else {
+      const signingKey = key.getPublicKey()
+      callback(null, signingKey)
+    }
+  })
+}
 
 module.exports = async (req, res) => {
   if (req.method !== 'GET') {
-    return res.status(405).json({ message: 'Method not allowed' });
+    return res.status(405).json({ message: 'Method not allowed' })
   }
 
-  const authHeader = req.headers.authorization;
+  const authHeader = req.headers.authorization
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
-    return res.status(401).json({ message: 'Unauthorized' });
+    return res.status(401).json({ message: 'Unauthorized' })
   }
 
-  const token = authHeader.split(' ')[1];
+  const token = authHeader.split(' ')[1]
 
-  try {
-    const response = await fetch(`https://${process.env.AUTH0_DOMAIN}/userinfo`, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
+  jwt.verify(
+    token,
+    getKey,
+    {
+      audience: process.env.AUTH0_AUDIENCE,
+      issuer: `https://${process.env.AUTH0_DOMAIN}/`,
+      algorithms: ['RS256']
+    },
+    async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({ message: 'Token verification failed', error: err.toString() })
+      }
 
-    if (!response.ok) {
-      return res.status(401).json({ message: 'Token verification failed' });
+      const uid = decoded.sub
+
+      try {
+        const userRef = db.collection('users').doc(uid)
+        const userDoc = await userRef.get()
+
+        if (!userDoc.exists) {
+          return res.status(404).json({ message: 'User not found' })
+        }
+
+        const publicData = userDoc.data()
+
+        return res.status(200).json({
+          ...publicData,
+          email: decoded.email,
+        })
+      } catch (error) {
+        return res.status(500).json({ message: 'Internal Server Error' })
+      }
     }
-
-    const user = await response.json();
-    const uid = user.sub;
-
-    const userRef = db.collection('users').doc(uid);
-    const userDoc = await userRef.get();
-
-    if (!userDoc.exists) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-
-    const publicData = userDoc.data();
-
-    return res.status(200).json({
-      ...publicData,
-      email: user.email,
-    });
-  } catch (error) {
-    return res.status(500).json({ message: 'Internal Server Error', error: error.toString() });
-  }
-};
+  )
+}
