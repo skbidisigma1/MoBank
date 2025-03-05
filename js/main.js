b9d23959a1a386f8c7936e41c8d40e5378227fb1
Luke Collingridge <luke.c309@stu.nebo.edu>
2025-03-05 00:16:21 +0000

    Update main.js
    ohh ok

--- js/main.js
+++ js/main.js
@@ -107,3 +107,16 @@ async function saveDontAskAgain(val) {
     };
   });
 }
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("ServiceWorker registration successful with scope: ", registration.scope);
+      })
+      .catch((error) => {
+        console.log("ServiceWorker registration failed: ", error);
+      });
+  });
+}
